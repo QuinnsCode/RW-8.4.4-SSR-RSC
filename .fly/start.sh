9c71c62439505acccda578c53d4431e8ec63d7d0
Ryan Quinn <notryanquinn@gmail.com>
2025-01-22 12:40:40 -0800

    RSC SSR app try 13

--- .fly/start.sh
+++ .fly/start.sh
@@ -8,6 +8,8 @@ fi
 
 # Debug: List what's in the bin directory
 ls -la /home/node/app/node_modules/.bin
+ls -la /home/node/app/node_modules/@redwoodjs/web/dist/cjs/bins/
+ls -la /home/node/app/node_modules/@redwoodjs/api-server/dist/
 
 # Start both frontend and API servers using .bin
 # node /home/node/app/node_modules/.bin/rw-serve-fe &
