6bb0c92a253da4f0fcfa81e57ae5e24b4ab197a1
Ryan Quinn <notryanquinn@gmail.com>
2025-01-22 11:33:50 -0800

    RSC SSR app try 9

--- .fly/start.sh
+++ .fly/start.sh
@@ -6,7 +6,9 @@ if [ -n $MIGRATE_ON_BOOT ]; then
   $(dirname $0)/migrate.sh
 fi
 
-# node ./node_modules/.bin/rw-serve-fe &
-# node ./node_modules/.bin/rw-server api
+# Debug: List what's in the bin directory
+ls -la /home/node/app/node_modules/.bin
 
-exec yarn rw serve
+# Start both frontend and API servers using .bin
+node /home/node/app/node_modules/.bin/rw-serve-fe &
+node /home/node/app/node_modules/.bin/rw-server api
